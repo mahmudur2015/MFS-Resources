7c6f43cc1dbd7cec4725b8cd9cdb6ef7c5ac1903
mahmudur2015 <sumonmahmud.2015@gmail.com>
2025-04-14 19:48:20 +0600

    main section design added

--- script/login.js
+++ script/login.js
@@ -5,7 +5,20 @@ document.getElementById("btn-login").addEventListener("click",
         event.preventDefault();
 
         const accountNumber = document.getElementById("txtAccountNumber").value;
+        const convertedaccountNumber = parseInt(accountNumber);
+        
         const pinNumber = document.getElementById("txtPinNumber").value;
-        console.log(accountNumber);
-        console.log(pinNumber);
+        const convertedPin = parseInt(pinNumber);
+
+        if (accountNumber.length === 11) {
+            if (convertedPin === 1234) {
+                window.location.href="./main.html"
+            }
+            else {
+                alert("Pin number not matched")
+            }
+        }
+        else {
+            alert("Need valid account number")
+        }
     })
